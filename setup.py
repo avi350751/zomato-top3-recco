0a1e48ae7938104d3a884789061da826d8ee4bb4
Avishek Bhattacharjee <avishek350751@gmail.com>
2023-12-15 18:48:29 -0600

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Top three food recommendations on zomato home screen',
+    author='Avishek Bhattacharjee',
+    license='',
+)
